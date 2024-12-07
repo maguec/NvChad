d0a6b10084cebedb0e76fc8c8763be97a3668963
Chris Mague <github@mague.com>
2024-12-07 07:37:50 -0800

    add snacks mainly for the dashboard

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -188,4 +188,17 @@ return {
       }
     end,
   },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      dashboard = { enabled = true },
+    },
+  }
 }
