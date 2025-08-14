d5c4c926d99d87d296ad838f7683f1ca33625851
Chris Mague <mague@google.com>
2025-08-14 17:03:29 -0400

    toggle vertical pipes as well

--- lua/custom/myfunctions.lua
+++ lua/custom/myfunctions.lua
@@ -5,10 +5,12 @@ if (vim.wo.number == false and vim.wo.relativenumber == false) then
      vim.wo.number = true
      vim.wo.relativenumber = true
      vim.wo.signcolumn = "yes:2"
+     vim.cmd('IBLToggle')
 elseif( vim.wo.number == true and vim.wo.relativenumber == true) then
      vim.wo.number = false
      vim.wo.relativenumber = false
      vim.wo.signcolumn = "no"
+     vim.cmd('IBLToggle')
 else
      vim.api.nvim_command('echo "something is broke yo!"')
 end
